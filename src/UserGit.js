18a801bffe6693bb44e22a5088ed2e7545cf938a
Vanessa <nessa1vane@icloud.com>
2021-10-26 20:09:12 -0300

    parte de informações do usuario

--- src/UserGit.js
+++ src/UserGit.js
@@ -2,7 +2,12 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from 'axios';
 import "./UserGit.css";
-import { Button, Input, InputGroup, InputGroupAddon } from 'reactstrap';
+import {
+    Button, Input, InputGroup, InputGroupAddon,
+    Container, Row, Col, Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Fade
+} from 'reactstrap';
+
 
 export default function User() {
 
@@ -12,6 +17,10 @@ export default function User() {
     const [image, setImage] = useState("");
     const [search, setSearch] = useState("");
     const [repo, setRepo] = useState([]);
+
+    // mostrar repositorios ao clicar
+    const [fadeIn, setFadeIn] = useState(false);
+    const toggle = () => setFadeIn(!fadeIn);
 
     function searchGit() {
         axios.get(`https://api.github.com/users/${search}`)
@@ -32,10 +41,6 @@ export default function User() {
                 setImage("nadinha");
             })
     }
-
-    /*function handleChange(event) {
-        setSearch(event.target.value);
-    }*/
 
     useEffect(() => {
 
@@ -63,17 +68,8 @@ export default function User() {
                 </InputGroup>
             </div>
 
-
-            {/* repositorios a direita e informações a esquerda 
-                - necessários criar divs
-                - cuidar do tamanho da imagem
-                - utilização de container -> grid
-            */}
-            <div className="card">
-                <img src={image} />
-                <h3>Nome: {name}</h3>
-                <h3>Bio: {bio}</h3>
-                <h3>Localização: {location}</h3>
+            <div>
+                {/*
 
                 <h3>Repositórios: </h3>
                 <div>
@@ -83,7 +79,41 @@ export default function User() {
                             <li>Descrição: {element.description}</li>
                         </ul>
                     ))}
-                </div>
+                </div> */}
+
+                <Container>
+                    <Row>
+                        {/* coluna da esquerda com as informações do usuario */}
+                        <Col>
+                            <Card>
+                                <div className="minhaImagem"><img width="100%" src={image} /></div>
+                                <CardBody>
+                                    <CardTitle tag="h5">Nome: {name}</CardTitle>
+                                    <CardSubtitle tag="h6" className="mb-2 text-muted">Biografia: {bio}</CardSubtitle>
+                                    <CardSubtitle tag="h6" className="mb-2 text-muted">Localização: {location}</CardSubtitle>
+                                    <Button style={{ marginTop: '10px' }} onClick={toggle}>Mostrar repositórios</Button>
+                                </CardBody>
+                            </Card>
+                        </Col>
+
+                        {/* coluna da direita que mostra os Repositórios */}
+                        <Col>
+                            <Card>
+                                <CardTitle tag="h5">Repositórios: </CardTitle>
+                                <Fade in={fadeIn}>
+
+                                    {/* Apenas os 6 primeiros repositórios devem ser exibidos, dispostos
+                                        numa tabela de cartões de 3 linhas e 2 colunas.
+                                    Os repositórios deverão exibir as seguintes informações: Título,
+                                        descrição e quantidade de estrelas. */}
+
+                                    
+
+                                </Fade>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         </>
     );
